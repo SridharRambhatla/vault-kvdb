6268bc0e4d914d469160724d1faadfbe14a3ebcb
Sridhar Rambhatla <52754188+SridharRambhatla@users.noreply.github.com>
2025-03-25 10:56:37 +0530

    Update db.go - Added create bucket function

--- db/db.go
+++ db/db.go
@@ -1,38 +1,66 @@
 package db
 
-import bolt "github.com/boltdb/bolt"
+import (
+	"fmt"
 
+	"github.com/boltdb/bolt"
+)
+
+// Database is an open bolt database.
 type Database struct {
 	db *bolt.DB
 }
 
-func NewDatabase(path string) (db *Database, closeFunc func() error, err error) {
-	boltdb, err := bolt.Open(path, 0600, nil)
+// NewDatabase returns an instance of a database that we can work with.
+func NewDatabase(dbPath string) (db *Database, closeFunc func() error, err error) {
+	boltDb, err := bolt.Open(dbPath, 0600, nil)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	closeFunc = boltdb.Close
+	db = &Database{db: boltDb}
+	closeFunc = boltDb.Close
 
-	return &Database{db: boltdb}, closeFunc, nil
+	// Optionally create default bucket
+	if err := db.CreateBucketIfNotExists("default"); err != nil {
+		closeFunc()
+		return nil, nil, fmt.Errorf("creating default bucket: %w", err)
+	}
+
+	return db, closeFunc, nil
 }
 
-// Set key function
-func (d *Database) setKey(key string, bucket, value []byte) error {
+// createBucketIfNotExists creates a bucket in the database if it doesn't exist.
+func (d *Database) CreateBucketIfNotExists(bucketName string) error {
 	return d.db.Update(func(tx *bolt.Tx) error {
-		b, _ := tx.CreateBucketIfNotExists([]byte(bucket))
+		_, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+		return err
+	})
+}
+
+// SetKey sets the key to the requested value in the specified bucket.
+func (d *Database) SetKey(key string, bucketName string, value []byte) error {
+	return d.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucketName))
+		if b == nil {
+			return fmt.Errorf("bucket %s not found", bucketName)
+		}
 		return b.Put([]byte(key), value)
 	})
 }
 
-// Get key function
-func (d *Database) getKey(bucket, key string) ([]byte, error) {
+// GetKey gets the value of the requested key from the specified bucket.
+func (d *Database) GetKey(bucketName string, key string) ([]byte, error) {
 	var result []byte
 	err := d.db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucket))
+		b := tx.Bucket([]byte(bucketName))
+		if b == nil {
+			return fmt.Errorf("bucket %s not found", bucketName)
+		}
 		result = b.Get([]byte(key))
 		return nil
 	})
+
 	if err == nil {
 		return result, nil
 	}
